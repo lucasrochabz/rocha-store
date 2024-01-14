262719d6463106b603ca112a85b45728d7172079
Lucas Rocha <lucasbezerrar@gmail.com>
2024-01-14 18:42:01 -0300

    Atualização do component MyProfile

--- src/pages/MyAccount/MyProfile/MyProfile.jsx
+++ src/pages/MyAccount/MyProfile/MyProfile.jsx
@@ -9,10 +9,6 @@ export const MyProfile = () => {
     navigate(path)
   }
 
-  const handleTest = () => {
-    navigate('/')
-  }
-
   return(
     <>
       <div>
@@ -20,21 +16,21 @@ export const MyProfile = () => {
         <h2>Olá [Nome], seja bem-vindo!</h2>
       </div>
       
-    <section className="box-content">
-      
-      <div className="box" onClick={() => handleNavigation('/meus-pedidos/meus-pedidos')}>
-        <h2>Meus Pedidos</h2>
-        <span>Rastrear, devolver ou comprar produtos novamente</span>
-      </div>
-
-      <div className="box" onClick={() => handleNavigation('/meus-pedidos/minhas-informacoes')}>
-        <h2>Minhas Informações</h2>
-      </div>
-
-        <div className="test-container" onClick={handleTest}>
-          <h1>teste</h1>
+      <section className="box-content">
+        
+        <div className="box" onClick={() => handleNavigation('/meus-pedidos/meus-pedidos')}>
+          <h2>Meus Pedidos</h2>
+          <span>Rastrear, devolver ou comprar produtos novamente</span>
         </div>
-    </section>
+
+        <div className="box" onClick={() => handleNavigation('/meus-pedidos/minhas-informacoes')}>
+          <h2>Minhas Informações</h2>
+        </div>
+
+        <div className="box" onClick={() => handleNavigation('/meus-pedidos/metodos-de-pagamento')}>
+          <h2>Métodos de Pagamento</h2>
+        </div>
+      </section>
     </>
   )
 }
